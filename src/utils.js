34fd789d9f68e0fd40b428e9a4615c6c6d4ab723
roberto <roberto1608torales@gmail.com>
2023-09-25 16:24:43 -0300

    se crearon los codigos para la autenticación de usuarios

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 import {fileURLToPath} from "url";
 import {dirname} from "path";
 import bcrypt from "bcrypt";
+import passport from "passport";
 
 export const createHash = (password) => {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(10)); 
@@ -10,6 +11,31 @@ export const isValidPassword = (user, password) => {
     return bcrypt.compareSync(password, user.password);
 } 
 
+export const passportCall = (strategy) => {
+    return async(req, res, next)=> {
+        passport.authenticate(strategy, function (error, user, info) {
+            if (error) return error;
+
+            if(!user){
+                return res.status(401).send({error:info.messages ? info.messages : info.toString()})
+            }
+        })(req, res, next);
+    }
+}
+
+export const authorization = (role) => {
+    return async (req, res, next) => {
+        if(!req.user){
+            return res.status(401).send({status:"error", message:"Unauthorizated"});
+        }
+
+        if(req.user.role != role){
+            return res.status(403).send({status:"error", message:"No permissions"});
+        }
+
+        next();
+    }
+}
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
